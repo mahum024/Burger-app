4fe5e04d34d63c1494309791aa7af2fd983fde98
mahum024 <mahum.maqbool@devsinc.com>
2022-10-25 19:18:42 +0500

    redux implemented

--- src/App.js
+++ src/App.js
@@ -2,92 +2,32 @@ import './App.css';
 import top from '../src/assets/top.jpg'
 import bottom from '../src/assets/bottom.jpg'
 import Ingredients from './components/Ingredients';
-import { useState, useReducer } from 'react';
 import { Stack } from '@mui/system';
+import React from 'react'
+import { addTomatoes } from '../src/redux'
+import { addCheese } from '../src/redux'
+import { addLettuse } from '../src/redux'
+import { addMeat } from '../src/redux'
+import { removeTomatoes } from '../src/redux'
+import { removeCheese } from '../src/redux'
+import { removeLettuse } from '../src/redux'
+import { removeMeat } from '../src/redux'
+import { connect } from 'react-redux'
 import BurgerFilling from './components/BurgerFilling';
 
-const initialState = 0
-
-const reducer = (state, action) => {
-  switch (action) {
-    case "increment":
-      return state + 1
-    case "decrement":
-      return state - 1
-    default :
-     break;
-  }
-}
-
-function App() {
-
-  const [tomatoCount, dispatchTomato] = useReducer(reducer, initialState)
-  const [meatCount, dispatchMeat] = useReducer(reducer, initialState)
-  const [lettuseCount, dispatchLettuse] = useReducer(reducer, initialState)
-  const [cheeseCount, dispatchCheese] = useReducer(reducer, initialState)
- 
-  const [totalAmount, setTotal] = useState(100)
- 
-  const Total = (price, item , method) => {
-    
-    setTotal(price)
-    switch (method) {
-      case "add":
-        switch (item) {
-          case "Meat":
-            dispatchMeat("increment")
-            break;
-          case "Lettuse":
-            dispatchLettuse("increment")
-            break;
-          case "Cheese":
-            dispatchCheese("increment")
-            break;
-          case "Tomato":
-            dispatchTomato("increment")
-            break;
-        
-          default:
-            break;
-        }
-        break;
-      case "remove":
-        switch (item) {
-          case "Meat":
-            dispatchMeat("decrement")
-            break;
-          case "Lettuse":
-            dispatchLettuse("decrement")
-            break;
-          case "Cheese":
-            dispatchCheese("decrement")
-            break;
-          case "Tomato":
-            dispatchTomato("decrement")
-            break;
-        
-          default:
-            break;
-        }
-      break;
-      default:
-        break;
-    }
-   
-  }
-
-
- return (
+function App(props) {
+  
+  return (
     
     <div className="App">
      <div className='BurgerContent' style={{overflowY:"scroll"}}>
         <Stack spacing={2}  >
           <img src=  {top}  alt=""/>
 
-          <BurgerFilling  name= "Tomato"  count= {tomatoCount} /> 
-          <BurgerFilling name= "Cheese"  count= {cheeseCount} />
-          <BurgerFilling name= "Meat"  count= {meatCount} />
-          <BurgerFilling name= "Lettuse"  count= {lettuseCount} />
+          <BurgerFilling  name= "Tomato"  count= {props.numOfTomatoes} /> 
+          <BurgerFilling name= "Cheese"  count= {props.numOfCheese} />
+          <BurgerFilling name= "Meat"  count= {props.numOfMeat} />
+          <BurgerFilling name= "Lettuse"  count= {props.numOfLettuse} />
          
           <img src=  {bottom} alt=""/>
         </Stack>
@@ -96,18 +36,41 @@ function App() {
       <div className='AddIngredients'>
         <div className='Ingredients'>
             <div className='CurrentPrice'>
-             Current Price: RS {totalAmount}
+             Current Price: RS {props.totalPrice}
             </div>
-            <Ingredients item="Tomato" price={10} total={Total} totalPrice={totalAmount}/>
-            <Ingredients item="Lettuse" price={20} total={Total} totalPrice={totalAmount}/>
-            <Ingredients item="Cheese" price={30} total={Total} totalPrice={totalAmount}/>
-            <Ingredients item="Meat" price={40} total={Total} totalPrice={totalAmount} />
-           
-
+            <Ingredients item="Tomatoes" add={props.addTomatoes} remove={props.removeTomatoes} />
+            <Ingredients item="Cheese" add={props.addCheese} remove={props.removeCheese} />
+            <Ingredients item="Meat" add={props.addMeat} remove={props.removeMeat} />
+            <Ingredients item="Lettuse" add={props.addLettuse} remove={props.removeLettuse} />
         </div>
       </div>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    numOfTomatoes: state.numOfTomatoes ,
+    numOfCheese: state.numOfCheese,
+    numOfLettuse: state.numOfLettuse ,
+    numOfMeat: state.numOfMeat, 
+    totalPrice: state.totalPrice
+  }
+}
+
+const mapDispatchToProps = dispatch =>{
+  return {
+    addTomatoes: () => dispatch( addTomatoes()),
+    addCheese: () => dispatch( addCheese()),
+    addMeat: () => dispatch( addMeat()),
+    addLettuse: () => dispatch( addLettuse()),
+
+    removeTomatoes: () => dispatch( removeTomatoes()),
+    removeCheese: () => dispatch( removeCheese()),
+    removeMeat: () => dispatch( removeMeat()),
+    removeLettuse: () => dispatch( removeLettuse()),
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { useReducer} from 'react';
 import { Button } from '@mui/material';
 import { ButtonGroup } from '@mui/material';
 import { Box } from '@mui/material';
@@ -17,36 +16,20 @@ const reducer = (state, action) => {
   }
 }
 export default function Ingredients(props){
-
-  const [count, dispatch] = useReducer(reducer, initialState)
- 
-  const Add = () => {
-    dispatch("increment")
-    props.total(props.price + props.totalPrice, props.item,  "add")
-  }
-  
-  const Remove = () => {
-    if(count > 0 ){
-
-      dispatch("decrement")
-      props.total( props.totalPrice - props.price , props.item, "remove")
-    }
-  }
-
-  return (
+ return (
     <div className='Ingredient'>
       <div className='item'>{props.item} </div> 
         <Box sx={{ display: 'flex', '& > *': { m: 1,  },  }} >
           <ButtonGroup orientation="vertical"
             aria-label="vertical outlined button group" >
-            <Button variant="contained" onClick={Add}>ADD</Button>
+            <Button variant="contained" onClick={props.add}>ADD</Button>
           </ButtonGroup>
           <ButtonGroup orientation="vertical"
             aria-label="vertical outlined button group" >   
-            <Button variant="contained" onClick={Remove} >Remove</Button>
+            <Button variant="contained" onClick={props.remove} >Remove</Button>
           </ButtonGroup>
         </Box>
-      <div>{count}</div>
+    
     </div>
   );
 }
